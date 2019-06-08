177aa1786b8dab5056846d28fbc3324ba8e19859
Ryosuke Akiyama <r@554.jp>
2019-06-08 17:49:26 +0900

    Add util package that includes utility functions

---
+++ util/util.go
@@ -0,0 +1,33 @@
+package util
+
+import "strings"
+
+// MentionToBot returns true when mentions include the bot userID.
+func MentionToBot(userID string, mentions []string) bool {
+	for _, m := range mentions {
+		if m == userID {
+			return true
+		}
+	}
+
+	return false
+}
+
+// HasKeywords find keywords in the text.
+func HasKeywords(text string, partial bool, keywords ...string) bool {
+	if partial {
+		for _, k := range keywords {
+			if strings.Contains(text, k) {
+				return true
+			}
+		}
+	} else {
+		for _, k := range keywords {
+			if text == k {
+				return true
+			}
+		}
+	}
+
+	return false
+}

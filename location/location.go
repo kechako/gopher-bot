3197281100c99ca4c0368d31928907fa65df1469
Ryosuke Akiyama <r@554.jp>
2019-09-10 13:48:50 +0900

    Fix a bug that "location.GetLocation" does not return "location not found" error

--- location/location.go
+++ location/location.go
@@ -2,14 +2,22 @@ package location
 
 import (
 	"context"
+	"errors"
 
 	"github.com/kechako/gopher-bot/location/internal/data"
+)
+
+var (
+	ErrLocationNotFound = errors.New("the location not found")
 )
 
 // GetLocation returns the latitude and longitude of the specified name.
 func GetLocation(ctx context.Context, name string) (lat, lon float32, err error) {
 	loc, err := data.GetLocation(ctx, name)
 	if err != nil {
+		if err == data.ErrKeyNotFound {
+			return 0, 0, ErrLocationNotFound
+		}
 		return
 	}
 

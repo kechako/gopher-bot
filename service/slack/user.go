b868d44d39d18a4ac09c1cf07fcea7621bf0498b
Ryosuke Akiyama <r@554.jp>
2019-09-24 22:10:22 +0900

    Add `FullName()' and `DisplayName()' to plugin.User

--- service/slack/user.go
+++ service/slack/user.go
@@ -1,8 +1,10 @@
 package slack
 
 type user struct {
-	id   string
-	name string
+	id          string
+	name        string
+	fullName    string
+	displayName string
 }
 
 // ID implements the plugin.User interface.
@@ -13,4 +15,20 @@ func (u *user) ID() string {
 // Name implements the plugin.User interface.
 func (u *user) Name() string {
 	return u.name
+}
+
+// FullName implements the plugin.User interface.
+func (u *user) FullName() string {
+	if u.fullName == "" {
+		return u.name
+	}
+	return u.fullName
+}
+
+// DisplayName implements the plugin.User interface.
+func (u *user) DisplayName() string {
+	if u.displayName == "" {
+		return u.FullName()
+	}
+	return u.displayName
 }
